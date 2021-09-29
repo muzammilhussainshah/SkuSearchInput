781918ccf605095bd2f86ef86e293a7b3b8bc65d
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2021-09-29 16:30:26 +0500

    search api integrate [ADD]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import SkuSearchInput from './components/SkuSearchInput';
 import FormResponse from './components/FormResponse';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [sku, setsku] = useState({});
+
   return (
     <div className="App" >
       <div className="col1">
-        <div style={{ width: "80%", }}>
           <SkuSearchInput
             searchUrl={'https://toro278.us-east.toroserver.com/api/demo_api_inventory/1.0/sku/search?'}
-            checkSearchKey={['sku_name','product_code']}
+            checkSearchKey={'sku_name'}
+            width={'80%'}
+            placeHolder={'Type to search for a service'}
+            backgroundColor={"#EEF1F0"}
+            onSelect={(value)=>setsku(value)}
           />
-        </div>
       </div>
       <div className="col2">
-        <FormResponse />
+        <FormResponse formData={sku} />
       </div>
     </div>
   );
--- src/components/FormResponse.js
+++ src/components/FormResponse.js
@@ -1,67 +1,67 @@
 import * as React from 'react';
-import '../App.css';
+import './App.css';
 
-export default function FormResponse() {
+export default function FormResponse({ sku }) {
   return (
     <div>
       <div className="Frm-Heading">
         Form Response
       </div>
 
-      <div style={{display:'flex',flexDirection:'row'}} >
+      <div style={{ display: 'flex', flexDirection: 'row' }} >
         <div className="Frm-Key-Val">
-         sku_id:
+          sku_id:
         </div>
         <div className="Frm-Key-Val" >
           51
         </div>
       </div>
-      <div style={{display:'flex',flexDirection:'row'}} >
+      <div style={{ display: 'flex', flexDirection: 'row' }} >
         <div className="Frm-Key-Val">
-        sku_name: 
+          sku_name:
         </div>
         <div className="Frm-Key-Val" >
-        Chisel Wood 6mm
+          Chisel Wood 6mm
         </div>
       </div>
-      <div style={{display:'flex',flexDirection:'row'}} >
+      <div style={{ display: 'flex', flexDirection: 'row' }} >
         <div className="Frm-Key-Val">
-        sku_name: 
+          sku_name:
         </div>
         <div className="Frm-Key-Val" >
-        Chisel Wood 6mm
+          Chisel Wood 6mm
         </div>
       </div>
-      <div style={{display:'flex',flexDirection:'row'}} >
+      <div style={{ display: 'flex', flexDirection: 'row' }} >
         <div className="Frm-Key-Val">
-        sku_name: 
+          sku_name:
         </div>
         <div className="Frm-Key-Val" >
-        Chisel Wood 6mm
+          Chisel Wood 6mm
         </div>
       </div>
-      <div style={{display:'flex',flexDirection:'row'}} >
+      <div style={{ display: 'flex', flexDirection: 'row' }} >
         <div className="Frm-Key-Val">
-        sku_name: 
+          sku_name:
         </div>
         <div className="Frm-Key-Val" >
-        Chisel Wood 6mm
+          Chisel Wood 6mm
         </div>
       </div>
-      <div style={{display:'flex',flexDirection:'row'}} >
+      <div style={{ display: 'flex', flexDirection: 'row' }} >
         <div className="Frm-Key-Val">
-        sku_name: 
+          sku_name:
         </div>
         <div className="Frm-Key-Val" >
-        Chisel Wood 6mm
+          Chisel Wood 6mm
         </div>
       </div>
-      <div style={{display:'flex',flexDirection:'row'}} >
+      <div style={{ display: 'flex', flexDirection: 'row' }} >
         <div className="Frm-Key-Val">
-        sku_name: 
+          sku_name:
         </div>
         <div className="Frm-Key-Val" >
-        Chisel Wood 6mm
+          Chisel Wood 6mm
         </div>
       </div>
 
