28c3e0c5e5d0e469e68a77e1b560a2bae7a8e3e7
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2021-09-29 09:06:34 +0500

    Layout [ADD]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import SkuSearchInput from './components/SkuSearchInput';
+import FormResponse from './components/FormResponse';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="col1">
+        <SkuSearchInput />
+      </div>
+      <div className="col2">
+        <FormResponse />
+      </div>
     </div>
   );
 }
---
+++ src/components/FormResponse.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import '../App.css';
+
+export default function FormResponse() {
+  return (
+    <div>
+      <div className="frmHeading">
+        Form Response
+      </div>
+
+      <div style={{display:'flex',flexDirection:'row'}} >
+        <div>
+          Issurer name
+        </div>
+        <div >
+          muzammil shah
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
